d6d1ba113e7644a52b346217f2cdb7f0dcca99a2
Vincat21 <43357640+Vincat21@users.noreply.github.com>
2018-12-04 16:40:12 -0800

    edited

--- src/main.java
+++ src/main.java
@@ -2,25 +2,20 @@ import java.util.Scanner;
 public class main {
 	public static void main(String[] args) {
 		boolean guessing = false;
-		System.out.println("Please enter a number.");
-		Scanner guessNum = new Scanner(System.in);
-		int num = guessNum.nextInt();
-		double x = Math.random();
-		x *= num;
-		x = Math.round(x);
+		System.out.println("Please enter a highest number.");
+		Scanner guess = new Scanner(System.in);
+		int x = Math.round(Math.random()* guess.nextInt());
 		guessing = true;
-		if(guessing) {
-			System.out.println("Guess a number between 1 and " + num + ".");
-			Scanner guess = new Scanner(System.in);
+		while(guessing) {
+			System.out.println("Guess a number between 1 and " + x + ".");
 			num = guess.nextInt();
-			guessing = false;
-		}
-		if(num == x) {
-			System.out.println("You guessed right!");
-		}
-		else {
-			System.out.println("You guessed wrong.");
-			guessing = true;
+			if(num == x) {
+				System.out.println("You guessed right!");
+				guessing = false;
+			}
+			else {
+				System.out.println("You guessed wrong.");
+			}
 		}
 	}
 
