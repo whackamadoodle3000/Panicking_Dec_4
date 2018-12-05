100cbbdd2b32dd4686b1acce169b984c52bfaa6f
AlexisELee <leea3909@lgsstudent.org>
2018-12-04 16:18:46 -0800

    init

---
+++ src/PlaysSound.java
@@ -0,0 +1,30 @@
+// This code done by Alexis
+import java.util.Scanner;
+import javax.sound.sampled.*; // imports sound
+class GuessingGame extends main { // subclass of Vincent's main guessing code
+	
+	public static void main (String[] args) {
+		new PlayingSound();
+	{
+		
+	public PlayingSound() {
+		
+		
+		
+		if (guesssing = false) { 
+			// if the user's guess is false play sound and enter "Wrong!"
+			System.out.println("Wrong!");
+			
+		}
+		
+		else {
+			
+			// don't play sound
+			System.out.println("You did it!");
+		}
+	}
+	
+	
+}
+	
+	// I tried to make it play sound but couldn't find out how to do it
